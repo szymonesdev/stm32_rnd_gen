841a89e073bcf21e0f43fb85e7bda142a2d462d3
szymonesdev <szymoneservice@gmail.com>
2017-11-14 11:27:58 +0100

    initial commit, working usb cdc, and temperature measure

---
+++ usb_cdc_adc/Source/app_src/src/main.c
@@ -0,0 +1,251 @@
+#include "stm32f4xx_hal.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+#include <stdio.h>
+
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+void SystemClock_Config(void);
+void _Error_Handler(char * file, int line);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_DMA_Init(void) ;
+
+uint16_t ADC_DUMP[2];
+
+int32_t DELAY_A;
+int32_t DELAY_B;
+char NL[2]= {'\n', '\r'};
+volatile char TX_DATA[256];
+volatile uint32_t len;
+uint32_t TERM_BIT0_1;
+uint32_t TERM_BIT0_0;
+uint32_t TERM_BIT1_1;
+uint32_t TERM_BIT1_0;
+int main(){
+
+	HAL_Init();
+  SystemClock_Config();
+	MX_GPIO_Init();
+  MX_USB_DEVICE_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_DUMP, 2);
+	
+	while(1){
+		DELAY_A= 1000;
+		
+		while( (DELAY_A--) > 0 ){
+			if( ADC_DUMP[0] & 0x1 )
+				TERM_BIT0_1++;
+			else
+				TERM_BIT0_0++;
+			
+			if( ADC_DUMP[0] & 0x2 )
+				TERM_BIT1_1++;
+			else
+				TERM_BIT1_0++;
+			
+			DELAY_B= 1000000;
+			while( (DELAY_B--) > 0 ){}
+		}
+		
+		len = sprintf(TX_DATA, "TEMPERATURE MEASURE \n\r BIT0 0: %d \n\r BIT0 1: %d \n\r BIT1 0: %d \n\r BIT1 1: %d \n\r", 
+		TERM_BIT0_0, TERM_BIT0_1, TERM_BIT1_0, TERM_BIT1_1);
+		CDC_Transmit_HS( (uint8_t*)TX_DATA, len );
+	}
+	
+	return 0;
+}
+
+ static void MX_GPIO_Init(void)
+{
+  /* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+/*
+void DMA2_Stream0_IRQHandler(){
+	//HAL_NVIC_DisableIRQ(DMA2_Stream0_IRQn);
+	DELAY= 10000000;
+	while(DELAY--){}
+	len = sprintf(NUMB, "%d \n\r", ADC_DUMP[0]);
+	CDC_Transmit_HS( (uint8_t*)NUMB, len );
+	len = sprintf(NUMB, "%d \n\r", ADC_DUMP[1]);
+	CDC_Transmit_HS( (uint8_t*)NUMB, len );
+	//HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+}*/
+
+static void MX_DMA_Init(void) 
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 1, 0);
+ // HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+}
+
+static void MX_ADC1_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = ENABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = 2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+ 
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+ 
+  /* The voltage scaling allows optimizing the power consumption when the
+     device is clocked below the maximum system frequency (see datasheet). */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+ 
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+ 
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 |
+                                RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspInit 0 */
+
+  /* USER CODE END ADC1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_ADC1_CLK_ENABLE();
+  
+    /**ADC1 GPIO Configuration    
+    PA1     ------> ADC1_IN1 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* ADC1 DMA Init */
+    /* ADC1 Init */
+    hdma_adc1.Instance = DMA2_Stream0;
+    hdma_adc1.Init.Channel = DMA_CHANNEL_0;
+    hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_adc1.Init.Mode = DMA_CIRCULAR;
+    hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_adc1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
+    {
+      _Error_Handler(__FILE__, __LINE__);
+    }
+
+    __HAL_LINKDMA(hadc,DMA_Handle,hdma_adc1);
+
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+  }
+
+}
+
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+{
+
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+  /* USER CODE END ADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC1_CLK_DISABLE();
+  
+    /**ADC1 GPIO Configuration    
+    PA1     ------> ADC1_IN1 
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
+
+    /* ADC1 DMA DeInit */
+    HAL_DMA_DeInit(hadc->DMA_Handle);
+  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+  /* USER CODE END ADC1_MspDeInit 1 */
+  }
+
+}
+
+void _Error_Handler(char * file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */ 
+}
---
+++ usb_cdc_adc/Source/app_src/src/usbd_cdc_if.c
@@ -0,0 +1,182 @@
+#include "usbd_cdc_if.h"
+#include "usb_device.h"
+#include "stm32f4xx_hal.h"
+
+#define APP_RX_DATA_SIZE  64
+#define APP_TX_DATA_SIZE  64
+
+uint8_t UserRxBufferHS[APP_RX_DATA_SIZE];
+uint8_t UserTxBufferHS[APP_TX_DATA_SIZE];
+extern USBD_HandleTypeDef hUsbDeviceHS;  
+extern PCD_HandleTypeDef hpcd_USB_OTG_HS;
+
+static int8_t CDC_Init_HS     (void);
+static int8_t CDC_DeInit_HS   (void);
+static int8_t CDC_Control_HS  (uint8_t cmd, uint8_t* pbuf, uint16_t length);
+static int8_t CDC_Receive_HS  (uint8_t* pbuf, uint32_t *Len);
+
+uint8_t CDC_Transmit_HS(uint8_t* Buf, uint16_t Len)
+{
+  uint8_t result = USBD_OK;
+  /* USER CODE BEGIN 12 */ 
+  USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceHS.pClassData;
+  if (hcdc->TxState != 0){
+    return USBD_BUSY;
+  }
+  USBD_CDC_SetTxBuffer(&hUsbDeviceHS, Buf, Len);
+  result = USBD_CDC_TransmitPacket(&hUsbDeviceHS);
+  /* USER CODE END 12 */ 
+  return result;
+}
+
+static int8_t CDC_Receive_HS (uint8_t* Buf, uint32_t *Len)
+{
+	//uint32_t a = *Len;
+	
+	//if( i ){
+		//if( i == 1 ){
+			//disp_putChar(Buf[0]);
+			//i--;
+		//}
+		//else{
+			//for( int b= 0; b < i; b++)
+				//disp_putChar(Buf[b]);
+			
+			//i= 0;
+		//}
+	//}
+
+  /* USER CODE BEGIN 11 */ 
+  USBD_CDC_SetRxBuffer(&hUsbDeviceHS, &Buf[0]);
+  USBD_CDC_ReceivePacket(&hUsbDeviceHS);
+  return (USBD_OK);
+  /* USER CODE END 11 */ 
+}
+
+void OTG_HS_IRQHandler(void)
+{
+		HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
+}
+
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+USBD_CDC_ItfTypeDef USBD_Interface_fops_HS = 
+{
+  CDC_Init_HS,
+  CDC_DeInit_HS,
+  CDC_Control_HS,  
+  CDC_Receive_HS
+};
+
+/**
+  * @brief  CDC_Init_HS
+  *         Initializes the CDC media low layer over the USB HS IP
+  * @param  None
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Init_HS(void)
+{ 
+  /* USER CODE BEGIN 8 */ 
+  /* Set Application Buffers */
+  USBD_CDC_SetTxBuffer(&hUsbDeviceHS, UserTxBufferHS, 0);
+  USBD_CDC_SetRxBuffer(&hUsbDeviceHS, UserRxBufferHS);
+  return (USBD_OK);
+  /* USER CODE END 8 */ 
+}
+
+/**
+  * @brief  CDC_DeInit_HS
+  *         DeInitializes the CDC media low layer
+  * @param  None
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_DeInit_HS(void)
+{
+  /* USER CODE BEGIN 9 */ 
+  return (USBD_OK);
+  /* USER CODE END 9 */ 
+}
+
+/**
+  * @brief  CDC_Control_HS
+  *         Manage the CDC class requests
+  * @param  cmd: Command code            
+  * @param  pbuf: Buffer containing command data (request parameters)
+  * @param  length: Number of data to be sent (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Control_HS  (uint8_t cmd, uint8_t* pbuf, uint16_t length)
+{ 
+  /* USER CODE BEGIN 10 */
+  switch (cmd)
+  {
+  case CDC_SEND_ENCAPSULATED_COMMAND:
+ 
+    break;
+
+  case CDC_GET_ENCAPSULATED_RESPONSE:
+ 
+    break;
+
+  case CDC_SET_COMM_FEATURE:
+ 
+    break;
+
+  case CDC_GET_COMM_FEATURE:
+
+    break;
+
+  case CDC_CLEAR_COMM_FEATURE:
+
+    break;
+
+  /*******************************************************************************/
+  /* Line Coding Structure                                                       */
+  /*-----------------------------------------------------------------------------*/
+  /* Offset | Field       | Size | Value  | Description                          */
+  /* 0      | dwDTERate   |   4  | Number |Data terminal rate, in bits per second*/
+  /* 4      | bCharFormat |   1  | Number | Stop bits                            */
+  /*                                        0 - 1 Stop bit                       */
+  /*                                        1 - 1.5 Stop bits                    */
+  /*                                        2 - 2 Stop bits                      */
+  /* 5      | bParityType |  1   | Number | Parity                               */
+  /*                                        0 - None                             */
+  /*                                        1 - Odd                              */ 
+  /*                                        2 - Even                             */
+  /*                                        3 - Mark                             */
+  /*                                        4 - Space                            */
+  /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
+  /*******************************************************************************/
+  case CDC_SET_LINE_CODING:   
+	
+    break;
+
+  case CDC_GET_LINE_CODING:     
+
+    break;
+
+  case CDC_SET_CONTROL_LINE_STATE:
+
+    break;
+
+  case CDC_SEND_BREAK:
+ 
+    break;    
+    
+  default:
+    break;
+  }
+
+  return (USBD_OK);
+  /* USER CODE END 10 */
+}
