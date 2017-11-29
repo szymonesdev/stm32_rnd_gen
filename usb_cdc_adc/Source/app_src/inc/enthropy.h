fb8ccecc42e698637162de3e18a7d580f2a948f2
MOJNICK <szymonszozda@gmail.com>
2017-11-29 15:02:55 +0100

    tmp comit

--- usb_cdc_adc/Source/app_src/inc/enthropy.h
+++ usb_cdc_adc/Source/app_src/inc/enthropy.h
@@ -1,4 +1,6 @@
 #ifndef ENTHROPY
 #define ENTHROPY
+#include "termometer_interface.h"
+#include "L3GD20_interface.h"
 	uint8_t* getRandomData(double minEnthropy);
 #endif
--- usb_cdc_adc/Source/app_src/src/enthropy.c
+++ usb_cdc_adc/Source/app_src/src/enthropy.c
@@ -3,22 +3,36 @@
 int const DATA_SIZE = 4096;//divisible by 4, max 4096, 4096 for current enthropy_data.h
 int const DATA_SIZE_4 = DATA_SIZE / 4;
 double const maxEnthropy = 8;
+const uint8_t u8Bits = 8;
+
+uint8_t getLSB(uint16_t number, uint8_t digits)
+{
+	number <<= 16 - digits;
+	number >>= 16 - digits;
+	return number;
+}
 
 uint8_t getTempByte()
 {
-	return 131;
+	uint8_t result = (uint8_t)(Termometer_getADCReading());
+	return result;
 }
 
 uint8_t* getgyroThreeByte()
 {
-	uint8_t* result = (uint8_t*)malloc(3);
-	return result;
+	L3GD20_XYZ_data_t* xyz_data = (L3GD20_XYZ_data_t*)malloc(sizeof(L3GD20_XYZ_data_t));
+	L3GD20_readXYZ(&xyz_data);
+	static uint8_t aryThree[3];
+	aryThree[0] = xyz_data->x_lsb;
+	aryThree[1] = xyz_data->y_lsb;
+	aryThree[2] = xyz_data->z_lsb;
+	return aryThree;
 }
 
 double calcEnthropy(uint8_t* data)
 {
-	uint16_t occurences[DATA_SIZE_4] = { 0 };
-	
+	static uint16_t occurences[DATA_SIZE_4] = { 0 };
+
 	for (int i = 0; i < DATA_SIZE_4; ++i)
 	{
 		++occurences[data[i]];
@@ -30,71 +44,96 @@ double calcEnthropy(uint8_t* data)
 		sum += _1024LOG2[occurences[i]];
 	}
 	double enthropy = -sum + 0.0000000001;
-	
+
+	memset(occurences, 0, DATA_SIZE_4);
 	return enthropy;
+}
+
+void pushToRandomArry(double dataEnthropy, uint8_t* data, double minEnthropy, uint8_t validBits)
+{
+	if (dataEnthropy >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
+	{
+		memcpy(randomArry, data, DATA_SIZE_4);
+		arryPos += DATA_SIZE_4;
+	}
 }
 
 
 
-uint8_t* getRandomData(double minEnthropy)
+uint8_t randomArry[DATA_SIZE * 2];// 2 prevent unallowed memory access
+int arryPos = 0; // mark: index, all higher and that one index are random valid
+
+uint8_t* fullFillRandomArry(double minEnthropy)
 {
-	static int counter = 0;//recursion number
+	static int counter = 0;//recursion number, stack overflow
 	++counter;
 
-	static uint8_t outputRandomData[DATA_SIZE];
-	static int arryPos = 0; // mark, all left elements are valid
-	
-	static uint8_t temp[DATA_SIZE_4];
-	static uint8_t gyroX[DATA_SIZE_4];
-	static uint8_t gyroY[DATA_SIZE_4];
-	static uint8_t gyroZ[DATA_SIZE_4];
+	static uint8_t temp[DATA_SIZE_4 * u8Bits];//only one bit could be valid so u8Bits max to full fill DATA_SIZE_4
+	static uint8_t gyroX[DATA_SIZE_4 * u8Bits];
+	static uint8_t gyroY[DATA_SIZE_4 * u8Bits];
+	static uint8_t gyroZ[DATA_SIZE_4 * u8Bits];
 	static uint8_t* gyroData;
 
-	for (int i = 0; i < DATA_SIZE_4; ++i)
+	static uint8_t tempBits = 4;//valid bits(LSB), expected to satisfy minEnthropy, i no decrease
+	static uint8_t gyroXbits = 4;
+	static uint8_t gyroYbits = 4;
+	static uint8_t gyroZbits = 4;
+
+	for (int i = 0; i < DATA_SIZE_4 * u8Bits / tempBits; ++i)
 	{
-		temp[i] = getTempByte();
+		temp[i] = getLSB(getTempByte(), tempBits);
 	}
 	for (int i = 0; i < DATA_SIZE_4; ++i)
 	{
 		gyroData = getgyroThreeByte();
-		gyroX[i] = gyroData[0];
-		gyroY[i] = gyroData[1];
-		gyroZ[i] = gyroData[2];
 	}
-		
+	for (int i = 0; i < DATA_SIZE_4; ++i)
+	{
+		gyroX[i] = getLSB(gyroData[0], gyroXbits);
+	}
+	for (int i = 0; i < DATA_SIZE_4; ++i)
+	{
+		gyroY[i] = getLSB(gyroData[1], gyroYbits);
+	}
+	for (int i = 0; i < DATA_SIZE_4; ++i)
+	{
+		gyroZ[i] = getLSB(gyroData[2], gyroZbits);
+	}
 	double eTemp = calcEnthropy(temp);
 	double eGyroX = calcEnthropy(gyroX);
 	double eGyroY = calcEnthropy(gyroY);
 	double eGyroZ = calcEnthropy(gyroZ);
-	
+
+	pushToRandomArry(eTemp, temp, minEnthropy);
+
 	if (eTemp >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
 	{
-		memcpy(outputRandomData, temp, DATA_SIZE_4);
+		memcpy(randomArry, temp, DATA_SIZE_4);
 		arryPos += DATA_SIZE_4;
 	}
 	if (eGyroX >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
 	{
-		memcpy(outputRandomData, gyroX, DATA_SIZE_4);
+		memcpy(randomArry, gyroX, DATA_SIZE_4);
 		arryPos += DATA_SIZE_4;
 	}
 	if (eGyroY >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
 	{
-		memcpy(outputRandomData, gyroY, DATA_SIZE_4);
+		memcpy(randomArry, gyroY, DATA_SIZE_4);
 		arryPos += DATA_SIZE_4;
 	}
 	if (eGyroZ >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
 	{
-		memcpy(outputRandomData, gyroZ, DATA_SIZE_4);
+		memcpy(randomArry, gyroZ, DATA_SIZE_4);
 		arryPos += DATA_SIZE_4;
 	}
 
 	if (arryPos >= DATA_SIZE)
-		return outputRandomData;
+		return randomArry;
 	else
 	{
 		if (counter > 10)
 			minEnthropy -= (maxEnthropy - minEnthropy);
 		minEnthropy = (minEnthropy > 0) ? (minEnthropy) : 0;
-		getRandomData(minEnthropy);
+		fullFillRandomArry(minEnthropy);
 	}
 }
