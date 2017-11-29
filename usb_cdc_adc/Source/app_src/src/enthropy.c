073f3e762a92ed64b607bae6f47000078f4def48
MOJNICK <szymonszozda@gmail.com>
2017-11-29 15:47:42 +0100

    fullFill finished, not tested

--- usb_cdc_adc/Source/app_src/src/enthropy.c
+++ usb_cdc_adc/Source/app_src/src/enthropy.c
@@ -5,6 +5,18 @@ int const DATA_SIZE_4 = DATA_SIZE / 4;
 double const maxEnthropy = 8;
 const uint8_t u8Bits = 8;
 
+uint8_t temp[DATA_SIZE_4 * u8Bits];//only one bit could be valid so u8Bits max to full fill DATA_SIZE_4
+uint8_t gyroX[DATA_SIZE_4 * u8Bits];
+uint8_t gyroY[DATA_SIZE_4 * u8Bits];
+uint8_t gyroZ[DATA_SIZE_4 * u8Bits];
+uint8_t* gyroData;
+
+uint8_t tempBits = 4;//HAVE TO BE EVEN// valid bits(LSB), expected to satisfy minEnthropy, if no decrease (divide by two)
+uint8_t gyroBits = 4;
+
+uint8_t randomArry[DATA_SIZE * 2];// 2 prevent unallowed memory access
+int arryPos = 0; // mark: index, all higher and that one index are random valid
+
 uint8_t getLSB(uint16_t number, uint8_t digits)
 {
 	number <<= 16 - digits;
@@ -49,24 +61,6 @@ double calcEnthropy(uint8_t* data)
 	return enthropy;
 }
 
-void pushToRandomArry(double dataEnthropy, uint8_t* data, double minEnthropy, uint8_t validBits)
-{
-	if (dataEnthropy >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
-	{
-		memcpy(randomArry, data, DATA_SIZE_4);
-		arryPos += DATA_SIZE_4;
-	}
-}
-
-uint8_t temp[DATA_SIZE_4 * u8Bits];//only one bit could be valid so u8Bits max to full fill DATA_SIZE_4
-uint8_t gyroX[DATA_SIZE_4 * u8Bits];
-uint8_t gyroY[DATA_SIZE_4 * u8Bits];
-uint8_t gyroZ[DATA_SIZE_4 * u8Bits];
-uint8_t* gyroData;
-
-uint8_t tempBits = 4;//valid bits(LSB), expected to satisfy minEnthropy, if no decrease (divide by two)
-uint8_t gyroBits = 4;
-
 void fullFillTemporaryArrys()
 {
 	for (int i = 0; i < DATA_SIZE_4 * u8Bits / tempBits; ++i)
@@ -107,10 +101,59 @@ void concatenateTemporaryArrys()
 	}
 }
 
-uint8_t randomArry[DATA_SIZE * 2];// 2 prevent unallowed memory access
-int arryPos = 0; // mark: index, all higher and that one index are random valid
+void pushIf(double minEnthropy)
+{
+	double eTemp = calcEnthropy(temp);
+	double eGyroX = calcEnthropy(gyroX);
+	double eGyroY = calcEnthropy(gyroY);
+	double eGyroZ = calcEnthropy(gyroZ);
 
-uint8_t* fullFillRandomArry(double minEnthropy)
+	uint8_t tmpGyroBits = gyroBits;//several /2
+
+	if (eTemp >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))//second prevent memory access violation
+	{
+		memcpy(randomArry, temp, DATA_SIZE_4);
+		arryPos += DATA_SIZE_4;
+	}
+	else
+	{
+		tempBits /= 2;
+	}
+
+	if (eGyroX >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
+	{
+		memcpy(randomArry, gyroX, DATA_SIZE_4);
+		arryPos += DATA_SIZE_4;
+	}
+	else
+	{
+		tmpGyroBits /= 2;
+	}
+
+	if (eGyroY >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
+	{
+		memcpy(randomArry, gyroY, DATA_SIZE_4);
+		arryPos += DATA_SIZE_4;
+	}
+	else
+	{
+		tmpGyroBits /= 2;
+	}
+
+	if (eGyroZ >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
+	{
+		memcpy(randomArry, gyroZ, DATA_SIZE_4);
+		arryPos += DATA_SIZE_4;
+	}
+	else
+	{
+		tmpGyroBits /= 2;
+	}
+
+	gyroBits == tmpGyroBits ? gyroBits = gyroBits : gyroBits /= 2;
+}
+
+void fullFillRandomArry(double minEnthropy)
 {
 	static int counter = 0;//recursion number, stack overflow
 	++counter;
@@ -118,36 +161,10 @@ uint8_t* fullFillRandomArry(double minEnthropy)
 	fullFillTemporaryArrys();
 	concatenateTemporaryArrys();
 
-	double eTemp = calcEnthropy(temp);
-	double eGyroX = calcEnthropy(gyroX);
-	double eGyroY = calcEnthropy(gyroY);
-	double eGyroZ = calcEnthropy(gyroZ);
-
-	pushToRandomArry(eTemp, temp, minEnthropy);
-
-	if (eTemp >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
-	{
-		memcpy(randomArry, temp, DATA_SIZE_4);
-		arryPos += DATA_SIZE_4;
-	}
-	if (eGyroX >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
-	{
-		memcpy(randomArry, gyroX, DATA_SIZE_4);
-		arryPos += DATA_SIZE_4;
-	}
-	if (eGyroY >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
-	{
-		memcpy(randomArry, gyroY, DATA_SIZE_4);
-		arryPos += DATA_SIZE_4;
-	}
-	if (eGyroZ >= minEnthropy && (DATA_SIZE - arryPos >= DATA_SIZE_4))
-	{
-		memcpy(randomArry, gyroZ, DATA_SIZE_4);
-		arryPos += DATA_SIZE_4;
-	}
+	pushIf(minEnthropy);
 
 	if (arryPos >= DATA_SIZE)
-		return randomArry;
+		return;
 	else
 	{
 		if (counter > 10)
